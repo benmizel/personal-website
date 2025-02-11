33c4da0006e62ba299d479f8f34ba904868da46d
Ben Mizel <bemizel@gmail.com>
2025-02-11 16:07:58 -0500

    Create and style about section of website.

---
+++ src/components/About/About.jsx
@@ -0,0 +1,20 @@
+import "./About.scss";
+
+const About = () => {
+  return (
+    <main className="about" id="about">
+      <div className="about-cont">
+        <div className="title-cont">
+          <h1 className="about__title">About Ben</h1>
+        </div>
+        <div className="desc-cont">
+          <h2 className="about__desc">
+          Born and raised in New York, Ben has always had a passion for solving complex problems. He received his bachelor degree from Hofstra University in Mathematical Economics and later earned an MBA from the Zicklin School of Business at Baruch College. Most recently, Ben completed a Software Engineering Bootcamp at BrainStation to broaden his technical skills. With over 8 years of experience across diverse industries, Ben combines technical expertise with strong business acumen and leadership abilities to bring value to any role. He’s known for his clear communication, strategic thinking, and collaborative approach to problem-solving. Outside of work, Ben enjoys playing video games and is an longtime fan of the New York Knicks and Jets!
+          </h2>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default About;
